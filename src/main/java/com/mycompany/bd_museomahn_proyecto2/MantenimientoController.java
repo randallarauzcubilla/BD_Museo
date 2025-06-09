5e4a321c53d88bb4c9da1805dc16369c3f5b8cb2
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-06-08 23:29:11 -0600

    Movido y actualizado MantenimientoController.java

--- src/main/java/com/mycompany/bd_museomahn_proyecto2/MantenimientoController.java
+++ src/main/java/com/mycompany/bd_museomahn_proyecto2/MantenimientoController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package com.mycompany.bd_museomahn_proyecto2;
 
 import java.net.URL;
