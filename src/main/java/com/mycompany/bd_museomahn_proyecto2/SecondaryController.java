436ba17cdf3ad07e463a4bd4328a466630ded902
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-06-08 22:59:05 -0600

    Recuperar archivos FXML y controladores borrados

---
+++ src/main/java/com/mycompany/bd_museomahn_proyecto2/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.mycompany.bd_museomahn_proyecto2;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
