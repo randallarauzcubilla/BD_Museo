2dd41acbcef48e56f9974e4bb1e8235f82d9dc55
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-06-09 20:10:24 -0600

    Renamed the persistence file for clarity and added the necessary dependencies to pom.xml

--- src/main/java/com/mycompany/bd_museomahn_proyecto2/App.java
+++ src/main/java/com/mycompany/bd_museomahn_proyecto2/App.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 /**
@@ -17,7 +16,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("Mantenimiento"), 1530, 1000);
         stage.setScene(scene);
         stage.show();
     }
@@ -34,5 +33,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
