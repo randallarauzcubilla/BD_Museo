ef84feafeee7842393c75d985932d49e4f61eb82
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-06-13 10:06:39 -0600

    Implemented CRUD operations for 'Collections' in JPA. Added methods for creating, retrieving, updating, and deleting collections within museum halls. Ensured proper database persistence and structured query handling.

--- src/main/java/controladores/ColeccionesJpaController.java
+++ src/main/java/controladores/ColeccionesJpaController.java
@@ -1,16 +1,87 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package controladores;
 
-import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import persistencia.Colecciones;
 
-/**
- *
- * @author Randall AC
- */
-public class ColeccionesJpaController implements Serializable {
+public class ColeccionesJpaController {
 
+    private EntityManagerFactory emf = null;
+
+    public ColeccionesJpaController() {
+        this.emf = Persistence.createEntityManagerFactory("MUSEO_JPA");
+    }
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    // Crear una nueva colección
+    public void create(Colecciones coleccion) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(coleccion);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    // Consultar todas las colecciones
+    public Collection<Colecciones> findColeccionesEntities() {
+        return findColeccionesEntities(true, 0, 0);
+    }
+
+    // Consultar colecciones con paginación
+    public Collection<Colecciones> findColeccionesEntities(int maxResults, int firstResult) {
+        return findColeccionesEntities(false, maxResults, firstResult);
+    }
+
+    private Collection<Colecciones> findColeccionesEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery<Colecciones> cq = em.getCriteriaBuilder().createQuery(Colecciones.class);
+            cq.select(cq.from(Colecciones.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return (Collection<Colecciones>) q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    // Actualizar datos de una colección
+    public void edit(Colecciones coleccion) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(coleccion);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    // Eliminar una colección
+    public void delete(Colecciones coleccion) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            Colecciones porEliminar = em.find(Colecciones.class, coleccion.getIdColeccion());
+            if (porEliminar != null) {
+                em.remove(porEliminar);
+                em.getTransaction().commit();
+            }
+        } finally {
+            em.close();
+        }
+    }
 }
