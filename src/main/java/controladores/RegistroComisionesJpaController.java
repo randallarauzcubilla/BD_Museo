76e93d204cb046edbbe19cc6ea2ed6a34caa020e
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-06-18 14:28:02 -0600

    Added create method to RegistroComisionesJpaController for saving commission records.

--- src/main/java/controladores/RegistroComisionesJpaController.java
+++ src/main/java/controladores/RegistroComisionesJpaController.java
@@ -1,14 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package controladores;
 
-/**
- *
- * @author Randall AC
- */
+import persistencia.RegistroComisiones;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class RegistroComisionesJpaController {
 
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("MUSEO_JPA");
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(RegistroComisiones comision) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(comision);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
 }
